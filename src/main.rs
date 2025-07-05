25f26fcebbf269e079fd508a5e862ea287cf3d47
DenTyur <denisturin1999@yandex.ru>
2025-07-05 12:36:39 +0300

    Коммит перед глобальным объединением калибровок

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,71 @@
-mod common;
-mod config;
-mod dim1;
-mod dim2;
-mod dim4;
-mod macros;
-mod potentials;
-mod traits;
-mod utils;
-use crate::common::tspace::Tspace;
-use crate::config::{C, F};
-use crate::dim2::{
+use rssfm::common::tspace::Tspace;
+use rssfm::config::F;
+use rssfm::dim2::ioniz_prob::IonizProb2D;
+use rssfm::dim2::{
     field::Field2D,
-    flow::{Flow2D, Square},
-    gauge::VelocityGauge2D,
-    space::{Pspace2D, Xspace2D},
+    gauge::{LenthGauge2D, VelocityGauge2D},
+    space::Xspace2D,
     ssfm::SSFM2D,
-    tsurff::Tsurff2D,
+    time_fft::TimeFFT,
     wave_function::WaveFunction2D,
 };
-use crate::potentials::{
-    absorbing_potentials::absorbing_potential_2d, potentials::br_1e2d_external,
+use rssfm::measure_time;
+use rssfm::potentials::absorbing_potentials::absorbing_potential_2d_asim;
+use rssfm::potentials::potentials;
+use rssfm::print_and_log;
+use rssfm::traits::{
+    flow::{Flux, SurfaceFlow},
+    space::Space,
+    ssfm::SSFM,
+    tsurff::Tsurff,
+    wave_function::WaveFunction,
 };
-use crate::traits::{space::Space, ssfm::SSFM, tsurff::Tsurff, wave_function::WaveFunction};
-use itertools::multizip;
-use ndarray::prelude::*;
-use rayon::prelude::*;
 use std::time::Instant;
 
 fn main() {
     // префикс для сохранения
-    let out_prefix = "out/dim2";
-    // пути к сохраненным массивам
-    let x_dir_path = "arrays_saved";
-    let psi_path = "arrays_saved/psi_initial.npy";
+    let out_prefix = "./out";
 
     // задаем параметры временной сетки
-    let mut t = Tspace::new(0., 0.2, 100, 25);
+    let mut t = Tspace::new(0., 0.2, 200, 40);
+    let save_step: usize = 1;
     t.save_grid(format!("{out_prefix}/time_evol/t.npy").as_str())
         .unwrap();
 
-    // задаем координатную сетку
-    let x_last = Xspace2D::load_from_npy(x_dir_path);
-
     // генерация начальной волновой функции psi
-    let mut psi = WaveFunction2D::init_from_npy(psi_path, x_last.clone());
-
-    // Расширяем сетку
-    let x = Xspace2D::new([-100.0, -100.0], x_last.dx, [400, 400]);
+    let psi_path = "/home/denis/disk_storage/DATA/br/br2e1d_N120_dx05_interact.hdf5";
+    let mut psi = WaveFunction2D::init_from_hdf5(psi_path);
+    // расширяем сетку
+    let x = Xspace2D::new([-100.0, -100.0], [0.5, 0.5], [2000, 2000]);
     psi.extend(&x);
     psi.normalization_by_1();
-    psi.plot_log("psi_init.png", [1e-8, 1.0]);
+    psi.plot_log(format!("{out_prefix}/psi_init.png").as_str(), [1e-8, 1.0]);
+    psi.x.save_as_npy(".");
 
-    // сохраняем импульсную сетку
-    psi.p.save_as_npy("arrays_saved/").unwrap();
+    // атомный и поглощающий потенциалы
+    let abs_pot = |x: [F; 2]| absorbing_potential_2d_asim(x, [[-50.0, 850.0], [-50.0, 850.0]], 0.4);
+    let atomic_pot = |x: [F; 2]| potentials::soft_coulomb_2e1d_interact(x, -1.0, 1.66, 2.6);
+
+    // вероятность двойной ионизации
+    let mut ioniz_prob = IonizProb2D::new(
+        [8.0, 10.0, 20.0, 30.0, 50.0, 100.0],
+        psi.x.clone(),
+        t.grid.clone(),
+    );
 
     // инициализируем внешнее поле
     let field = Field2D {
-        amplitude: 0.038,
-        omega: 0.04,
+        amplitude: 0.035,
+        omega: 0.0018849555921538759,
         N: 3.0,
-        x_envelop: 30.0001,
+        x_envelop: 1000.0001,
     };
+
     // указываем калибровку поля
-    let gauge = VelocityGauge2D::new(&field);
+    let gauge = LenthGauge2D::new(&field);
 
     // инициализируем структуру для SSFM эволюции (решатель ур. Шредингера)
-    let abs_pot = |x: [F; 2]| absorbing_potential_2d(x, 50.0, 0.02);
-    let mut ssfm = SSFM2D::new(&gauge, &x, br_1e2d_external, abs_pot);
-
-    // создаем структуру для потока вероятности через поверхность
-    let surface = Square::new(20.0, &x);
-    let mut flow = Flow2D::new(&gauge, &surface);
-
-    // t-surff
-    // let mut tsurff = Tsurff2D::new(&gauge, &surface, &p, Some(3.0));
+    let mut ssfm = SSFM2D::new(&gauge, &psi.x, atomic_pot, abs_pot);
 
     // эволюция
     let total_time = Instant::now();
@@ -91,42 +83,19 @@ fn main() {
         //                       SSFM
         //============================================================
         measure_time!("SSFM", {
-            ssfm.time_step_evol(
-                &mut psi, &mut t,
-                None,
-                // Some(f!("arrays_saved/time_evol/psi_x/psi_x_t_{i}.npy").as_str()),
-                // Some(f!("arrays_saved/time_evol/psi_p/psi_p_t_{i}.npy").as_str()),
-            );
+            ssfm.time_step_evol(&mut psi, &mut t, None);
         });
         // график волновой функции
-        // if i % 100 == 0 {
-        psi.plot_log(
-            format!("{out_prefix}/imgs/time_evol/psi_x/psi_x_t_{i}.png").as_str(),
-            [1e-8, 1.0],
-        );
-        // }
-        //обновление производных
-        measure_time!("update_deriv", {
-            psi.update_derivatives();
-        });
-        //============================================================
-        //                       t-SURFF
-        //============================================================
-        // measure_time!("tsurff", {
-        //     tsurff.time_integration_step(&psi, &t);
-        //     if i % 100 == 0 {
-        //         tsurff.plot_log(
-        //             format!("{out_prefix}/tsurff/tsurff{i}.png").as_str(),
-        //             [1e-5, 1.0],
-        //         );
-        //     }
-        // });
-        //============================================================
-        //                       Flow
-        //============================================================
-        measure_time!("flow_time", {
-            flow.add_instance_flow(&psi, t.current);
-        });
+        if save_step == 1 || i % save_step == 0 {
+            psi.plot_log(
+                format!("{out_prefix}/imgs/time_evol/psi_x/psi_x_t_{i}.png").as_str(),
+                [1e-8, 1.0],
+            );
+            //сохранение в.ф.
+            psi.save_as_npy(format!("{out_prefix}/time_evol/psi_x/psi_t_{i}.npy").as_str());
+        }
+        // Ioniz Prob
+        ioniz_prob.add(&psi.psi);
         //============================================================
         print_and_log!(
             "time_step = {:.3}, total_time = {:.3}",
@@ -135,11 +104,6 @@ fn main() {
         )
     }
 
-    flow.plot_flow(format!("{out_prefix}/imgs/flow.png").as_str());
-    let total_flow = flow.compute_total_flow(t.t_step());
-    print_and_log!("total_flow = {}", total_flow);
-    print_and_log!(
-        "total_flow + prob_in_box = {}",
-        total_flow.re + psi.prob_in_numerical_box()
-    );
+    ioniz_prob.plot(&t.grid, "ioniz_prob.png");
+    ioniz_prob.save_as_hdf5("ioniz_prob.hdf5");
 }
